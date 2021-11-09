c00bc19f660ee5cad78be5797dd68cb2e954a556
Andrew Cate <acate888@gmail.com>
2021-11-09 12:27:35 -0500

    Feature Engineering. Merging features into modeling table.

--- CustomerDataCleaning.sql
+++ CustomerDataCleaning.sql
@@ -132,13 +132,13 @@ true, delimiter ";");
 
 
 -- Customer with maximum number of entries in Complaints Table 
---are more likely to churn out
+-- are more likely to churn out
 select count(CustomerID) as NumComplaints, CustomerID  from complaints
 GROUP BY CustomerID
 ORDER BY NumComplaints DESC;
 
 -- Customers associated with the Product IDs with maximum numbers of 
--- complaints are likely to churn out
+-- complaints are likely to churn out 
 select count(ProductID), ProductID from complaints GROUP BY ProductID;
 
 -- Customers with NULL Renewal date in Subscription table are more likely
@@ -176,7 +176,62 @@ where customerid not in (select customerid
 -- 364 Churn
 -- 1004 Not Churn
 
--- Next Steps
--- Data Engineering to find predictors of Churn
--- 
+drop table if exists referenceTbl ;
+create table referenceTbl as (
+    select * from subscriptions where startdate < '2010-01-05'
+) ;
 
+-- churn case when logic
+create table churnTbl as (
+select distinct customerid,
+        case when customerid in (
+            select distinct customerid 
+from (  select customerid 
+        from subscriptions 
+        where customerid not in (   select customerid 
+                                    from subscriptions 
+                                    where '2010-01-05' between startdate and enddate ) -- Active Subscriptions
+     ) as churnTbl -- Select customerid which do not have an active subscription at 2010-01-05
+where customerid not in (select customerid 
+                         from (select customerid, min(startdate) ,min(startdate) > '2010-01-05' as AfterReference 
+                               from subscriptions group by customerid) as AfterReferenceTbl -- customers who joined after the reference date
+                         where AfterReference = True) -- filter out customers who joined after the reference date
+
+        ) then TRUE
+             else FALSE
+        end as churn
+from referenceTbl 
+) ;
+
+-- Number of products by customer
+select customerid, count(distinct(productid)) from subscriptions group by customerid;
+
+-- Customer Lifetime , this would fail if they had a gap
+select customerid, max(enddate) - min(startdate) as CustomerLifetime from subscriptions group by customerid;
+
+-- Number of subscriptions for each customer
+select customerid, count(distinct(subscriptionid)) as NumSubscriptions from subscriptions group by customerid;
+
+-- Total Price paid by each customer
+select customerid, sum(totalprice) as TotalPaid from subscriptions group by customerid ;
+
+
+-- Merging churn table with predictors, this table we can use for modeling
+select churnTbl.customerid, churn, NumProducts, CustomerLifetime, NumSubscriptions, TotalPaid from churnTbl 
+left outer join (select customerid, count(distinct(productid)) as NumProducts
+                 from subscriptions 
+                 group by customerid) as NumProdTbl
+on churnTbl.customerid = NumProdTbl.customerid 
+left outer join (select customerid, max(enddate) - min(startdate) as CustomerLifetime 
+                 from subscriptions 
+                 group by customerid) as CustomerLifetimeTbl
+on churnTbl.customerid = CustomerLifetimeTbl.customerid
+left outer join (select customerid, count(distinct(subscriptionid)) as NumSubscriptions 
+                 from subscriptions 
+                 group by customerid) as NumSubscriptionsTbl
+on churnTbl.customerid = NumSubscriptionsTbl.customerid 
+left outer join (select customerid, sum(totalprice) as TotalPaid 
+                 from subscriptions 
+                 group by customerid) as TotalPaidTbl
+on churnTbl.customerid = TotalPaidTbl.customerid
+;
