9428efea291e4edaf05bbfb46c6d1310fec15db3
Isaac Sheets <isheets@vols.utk.edu>
2021-11-19 16:26:40 -0500

    Confusion Matricies and Naive Accuracy Creation

--- isheets_CustomerDefectionModel.py
+++ isheets_CustomerDefectionModel.py
@@ -6,10 +6,14 @@ import numpy as np
 from sklearn import metrics 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Transferring data from PostgreSQL
 
 database = {
             'user' : 'postgres',
-            'pass' : '100Million',
+            'pass' : 'book',
             'name' : 'postgres',
             'host' : 'localhost',
             'port' : '5432'}
@@ -27,6 +31,8 @@ result = pd.read_sql_query(query, pgConnection)
 pgConnection.close()
 print(result)
 
+#Creating Test/Train
+
 x = result.drop('churn',axis = 1)
 y = result.churn
 
@@ -38,6 +44,48 @@ logistic_regression.fit(x_train,y_train)
 
 y_pred = logistic_regression.predict(x_test)
 
-accuracy = metrics.accuracy_score(y_test, y_pred)
-accuracy_percentage = 100 * accuracy
-accuracy_percentage
+#Confusion Matrix and Accuracy for Predictions
+
+score = logistic_regression.score(x_test, y_test)
+print(score)
+
+cm = metrics.confusion_matrix(y_test, y_pred)
+print(cm)
+
+plt.figure(figsize=(2,2))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Oranges')
+plt.ylabel('Actual Churn')
+plt.xlabel('Predicted GLM Churn')
+all_sample_title = 'Accuracy Score: {0}'.format(score)
+plt.title(all_sample_title, size = 15)
+plt.show()
+
+#Determining/Building Naive Predictions
+
+sum(y_test==False)
+len(y_test==False)
+
+naive_pred = np.full(
+  shape=342,
+  fill_value=False,
+  dtype=np.bool_
+)
+
+#Confusion Matrix for Naive Predictions
+
+cm2 = metrics.confusion_matrix(y_test, naive_pred)
+print(cm2)
+
+score2 = logistic_regression.score(x_test, naive_pred)
+print(score2)
+
+plt.figure(figsize=(2,2))
+sns.heatmap(cm2, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Oranges')
+plt.ylabel('Actual Churn')
+plt.xlabel('Predicted Naive Churn')
+all_sample_title = 'Accuracy Score: {0}'.format(score2)
+plt.title(all_sample_title, size = 15)
+plt.show()
+
+score #Log Reg Model
+score2 #Naive
